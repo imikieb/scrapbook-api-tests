30e313e75142e09e73ad37722b1f5033cc3cfb1a
imikieb <mikie.business99@gmail.com>
2022-06-24 16:07:51 -0300

    sqlite config

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,39 @@
 require('dotenv').config();
 
-module.exports = {
-    type: process.env.DATABASE_TYPE,
-    url: process.env.DATABASE_URL,
-    logging: false,
-    extra: {
-        ssl: {
-            rejectUnauthorized: false
+let config = {};
+
+if (process.env.NODE_ENV?.toLowerCase() === 'test') {
+    config = {
+        type: 'sqlite',
+        database: './db-test.sql',
+        migrations: [
+            'src/database/migrations/**/*'
+        ],
+        entities: [
+            'src/database/entities/**/*'
+        ]
+    };
+} else {
+    config = {
+        type: process.env.DATABASE_TYPE,
+        url: process.env.DATABASE_URL,
+        logging: false,
+        extra: {
+            ssl: {
+                rejectUnauthorized: false
+            }
+        },
+        migrations: [
+            'src/database/migrations/**/*'
+        ],
+        entities: [
+            'src/database/entities/**/*'
+        ],
+        cli: {
+            entitiesDir: 'src/database/entities',
+            migrationsDir: 'src/database/migrations'
         }
-    },
-    migrations: [
-        'src/database/migrations/**/*.ts'
-    ],
-    entities: [
-        'src/database/entities/**/*.ts'
-    ],
-    cli: {
-        entitiesDir: 'src/database/entities',
-        migrationsDir: 'src/database/migrations'
-    }
+    };
 }
+
+module.exports = config;
